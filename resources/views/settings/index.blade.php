c9733fa18730c3538dc8d74724d5e4ef83e5085c
Abdul Kholik <engineertekno@gmail.com>
2026-03-06 23:55:23 +0700

    feat: Add new school settings fields for NPSN, address, contact, and principal details, and adjust form column layout.

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -14,17 +14,22 @@
                 @csrf
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-md-4">
                             <div class="form-group">
                                 <label>Pemerintah Kota/Yayasan</label>
                                 <input type="text" name="school_city" class="form-control" value="{{ $settings['school_city'] ?? '' }}" placeholder="PEMERINTAH KOTA ADMINISTRASI SEKOLAH">
-                                <small class="text-muted">Muncul di baris pertama KOP surat.</small>
                             </div>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-4">
                             <div class="form-group">
                                 <label>Nama Sekolah</label>
                                 <input type="text" name="school_name" class="form-control" value="{{ $settings['school_name'] ?? '' }}" placeholder="SMK NEGERI CONTOH SISARPA">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label>NPSN Sekolah</label>
+                                <input type="text" name="school_npsn" class="form-control" value="{{ $settings['school_npsn'] ?? '' }}" placeholder="12345678">
                             </div>
                         </div>
                     </div>
@@ -35,13 +40,46 @@
                     </div>
 
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label>Kecamatan</label>
+                                <input type="text" name="school_district" class="form-control" value="{{ $settings['school_district'] ?? '' }}">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label>Kabupaten/Kota</label>
+                                <input type="text" name="school_regency" class="form-control" value="{{ $settings['school_regency'] ?? '' }}">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label>Provinsi</label>
+                                <input type="text" name="school_province" class="form-control" value="{{ $settings['school_province'] ?? '' }}">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label>Kode Pos</label>
+                                <input type="text" name="school_zip" class="form-control" value="{{ $settings['school_zip'] ?? '' }}">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label>Telepon</label>
+                                <input type="text" name="school_phone" class="form-control" value="{{ $settings['school_phone'] ?? '' }}">
+                            </div>
+                        </div>
+                        <div class="col-md-4">
                             <div class="form-group">
                                 <label>Website</label>
                                 <input type="text" name="school_website" class="form-control" value="{{ $settings['school_website'] ?? '' }}">
                             </div>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-4">
                             <div class="form-group">
                                 <label>Email Sekolah</label>
                                 <input type="email" name="school_email" class="form-control" value="{{ $settings['school_email'] ?? '' }}">
@@ -49,6 +87,24 @@
                         </div>
                     </div>
 
+                    <hr>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Nama Kepala Sekolah</label>
+                                <input type="text" name="school_principal_name" class="form-control" value="{{ $settings['school_principal_name'] ?? '' }}" placeholder="Dr. Contoh Nama, M.Pd">
+                                <small class="text-muted">Digunakan untuk tanda tangan laporan.</small>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>NIP Kepala Sekolah</label>
+                                <input type="text" name="school_principal_nip" class="form-control" value="{{ $settings['school_principal_nip'] ?? '' }}" placeholder="19800101 200501 1 001">
+                            </div>
+                        </div>
+                    </div>
+
+                    <hr>
                     <div class="form-group">
                         <label>Logo Sekolah</label>
                         @php $logo = $settings['school_logo'] ?? 'default_logo.png'; @endphp
